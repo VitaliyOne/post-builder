7c14541beb78e05903c155ed4cf19487d3d3e9b2
VitaliyOne <vitaliy.one1@yandex.ru>
2023-09-19 23:10:07 +0300

    Code review

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,7 +4,7 @@ import PostsItem from './PostsItem';
 const PostList = ({ posts, titlePosts, remove }) => {
   return (
     <div>
-      <h2 style={{ textAlign: 'center' }}>{titlePosts}</h2>
+      <h1 style={{ textAlign: 'center' }}>{titlePosts}</h1>
       {posts.map((post, index) => (
         <PostsItem remove={remove} number={index + 1} post={post} key={post.id} />
       ))}
